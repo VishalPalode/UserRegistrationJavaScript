fa17e06d4f530f9f2c6d4bfda1c4bb07b2f0ae1b
VishalPalode <96374249+VishalPalode@users.noreply.github.com>
2022-05-10 13:52:25 +0530

    Update helper.js

--- helper.js
+++ helper.js
@@ -1,6 +1,6 @@
 module.exports={
     nameRegex: RegExp(/^[A-Z][a-z]{3,}$/),
-    contactRegex: RegExp(/^[+91]\d{10}$/),
+    contactRegex: RegExp(/^[7-9][0-9]{9}$/),
     emailRegex: RegExp(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/),
     passwordRegex: RegExp(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/)
 }
